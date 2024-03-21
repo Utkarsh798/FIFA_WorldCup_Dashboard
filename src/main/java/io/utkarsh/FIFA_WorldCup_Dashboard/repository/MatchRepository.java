3b7dcad8b743b33c22220a1559c642207e7bbe59
Utkarsh798 <utkarshamin98@gmail.com>
2024-03-21 10:43:34 -0500

    backend build new API for all the match information by dates

--- src/main/java/io/utkarsh/FIFA_WorldCup_Dashboard/repository/MatchRepository.java
+++ src/main/java/io/utkarsh/FIFA_WorldCup_Dashboard/repository/MatchRepository.java
@@ -1,6 +1,5 @@
 package io.utkarsh.FIFA_WorldCup_Dashboard.repository;
 
-import io.utkarsh.FIFA_WorldCup_Dashboard.DataProcess.StageMatch;
 import io.utkarsh.FIFA_WorldCup_Dashboard.model.Matches;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,12 +10,9 @@ import java.util.List;
 public interface MatchRepository extends JpaRepository<Matches,Long> {
 
 
-    @Query("select distinct m.stageName, m.date from Matches m where m.year = :year order by m.date")
-    Object[] getAllDistinctStageName(@Param("year") String year);
 
-    @Query("SELECT new io.utkarsh.FIFA_WorldCup_Dashboard.DataProcess.StageMatch(m.id,m.stageName, m.home_team, m.away_team, m.home_score, m.away_score, m.winning_team, m.losing_team, m.date, m.home_penalty, m.away_penalty) " +
-            "FROM Matches m WHERE m.year = :year ORDER BY m.date ASC ")
-    List<StageMatch> getAllMatchList(@Param("year") String year);
+    @Query("select m from Matches m where m.year = :year order by m.date")
+    List<Matches> getAllMatchByDate(@Param("year") String year);
 
 
 }
